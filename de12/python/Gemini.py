294afe6608bb89d40aa06747039d7e24729c2e73
shinoshino39 <r202502031ty@jindai.jp>
2025-11-04 22:21:31 +0900

    20251104

---
+++ de12/python/Gemini.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key="AIzaSyBJ6Djr3X8utJQZNTJP057_pP2ZEvsIMXk")
+
+gemini_pro = genai.GenerativeModel("gemini-pro")
+
+prompt = "こんにちは"
+response = gemini_pro.generate_content(prompt)
+print(response.text)
